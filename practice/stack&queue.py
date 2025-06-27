74356a23fd8a686e6c658bdb1daa3b406883491b
Akul-singh831 <akulsingh831@gmail.com>
2025-06-27 10:51:52 +0530

    Create stack&queue.py

---
+++ practice/stack&queue.py
@@ -0,0 +1,13 @@
+#stack
+list = [1, 2, 3, 4, 5, 6]
+list.append(9)
+list.pop()
+top = list[-1]
+print(top)
+print(list)
+#queue
+list2 = [56, 68, 34, 53, 27, 75]
+list2.append(99)
+list2.pop(0)
+print(list2[0])
+print(list2)
